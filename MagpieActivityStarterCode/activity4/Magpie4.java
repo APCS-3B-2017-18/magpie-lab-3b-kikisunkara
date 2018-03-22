32504a67b4124cf1f3cadf70970f88caaa731866
kikisunkara <37308990+kikisunkara@users.noreply.github.com>
2018-03-22 11:29:28 -0700

    Update Magpie4.java

--- MagpieActivityStarterCode/activity4/Magpie4.java
+++ MagpieActivityStarterCode/activity4/Magpie4.java
@@ -1,4 +1,4 @@
-package magpie;
+package magpie2;
 
 /**
  * A program to carry on conversations with a human user.
@@ -102,6 +102,25 @@ public class Magpie4
   return "Would you really be happy if you had " + restOfStatement + "?";
  }
 
+ 
+ private String transformIWantStatement(String statement)
+ {
+  //  Remove the final period, if there is one
+  statement = statement.trim();
+  String lastChar = statement.substring(statement
+    .length() - 1);
+  if (lastChar.equals("."))
+  {
+   statement = statement.substring(0, statement
+     .length() - 1);
+  }
+  int psn = findKeyword (statement, "I want", 0);
+  String restOfStatement = statement.substring(psn + 7).trim();
+  return "Why do you want " + restOfStatement + "?";
+ }
+ 
+ 
+ 
  
  
  /**
